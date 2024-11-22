8fd0ecb689a988d185d6c817e87416d9f3b6514f
Jeff Tang <jeffrey.sj.tang@gmail.com>
2024-11-21 21:02:29 -0800

    debug and fix download_reference process

---
+++ work/3d/179e6876ac66e76332f1559501874a/.command.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -ue
+wget -O gencode.vM36.basic.annotation.gtf.gz https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M36/gencode.vM36.basic.annotation.gtf.gz
---
+++ work/75/8c421266d6a4737a3e67045a723335/.command.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -ue
+wget -O gencode.vM36.transcripts.fa.gz https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M36/gencode.vM36.transcripts.fa.gz
