13f3531e24e206e2e92cf597913c4dfbe5a35a11
Jeff Tang <jeffrey.sj.tang@gmail.com>
2024-11-22 15:37:08 -0800

    remove cached files that should have been ignored

--- work/3d/179e6876ac66e76332f1559501874a/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-wget -O gencode.vM36.basic.annotation.gtf.gz https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M36/gencode.vM36.basic.annotation.gtf.gz
--- work/75/8c421266d6a4737a3e67045a723335/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-wget -O gencode.vM36.transcripts.fa.gz https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M36/gencode.vM36.transcripts.fa.gz
